7626c2d8842d4300668444d72afa27b434e4475c
Gkekas Dimitris <dimigkek@gmail.com>
2022-02-14 15:39:56 +0200

    Update Game 1.py

--- Game 1.py
+++ Game 1.py
@@ -8,6 +8,12 @@ while again != "no" or again != "No" :
     while difficulty != "Easy" and difficulty != "Normal" and difficulty != "Hard" and difficulty != "easy" and difficulty != "normal" and difficulty != "hard":
         print("Choose between Easy , Normal and Hard")
         difficulty = input("Choose a difficulty level : ")
+    if difficulty == "easy" or difficulty == "Easy":
+        print("Easy mode activated you have 10 tries , good luck! ")
+    elif difficulty == "normal" or difficulty == "Normal":
+        print("Normal mode activated you have 5 tries , good luck! ")
+    else:
+        print("Hard mode activated you have 3 tries , good luck! ")
     guessed_number = int(input("Guess the number between 1 and 100 : "))
     y = 0
     while hidden != guessed_number:
@@ -18,10 +24,10 @@ while again != "no" or again != "No" :
                 break
             if guessed_number < hidden:
                 print("The number we are looking for is bigger than yours")
-                guessed_number = int(input("Try again \n"))
+                guessed_number = int(input("Try again, you still have ("+ str(10 - count) + ") try/tries! \n"))
             elif guessed_number > hidden:
                 print("The number we are looking for is smaller than yours")
-                guessed_number = int(input("Try again \n"))
+                guessed_number = int(input("Try again, you still have ("+ str(10 - count) + ") try/tries! \n"))
         elif difficulty == "Normal" or difficulty == "normal":
             count += 1
             if count == 5:
@@ -29,10 +35,10 @@ while again != "no" or again != "No" :
                 break
             if guessed_number < hidden:
                 print("The number we are looking for is bigger than yours")
-                guessed_number = int(input("Try again \n"))
+                guessed_number = int(input("Try again, you still have ("+ str(5 - count) + ") try/tries! \n"))
             elif guessed_number > hidden:
                 print("The number we are looking for is smaller than yours")
-                guessed_number = int(input("Try again \n"))
+                guessed_number = int(input("Try again, you still have ("+ str(5 - count) + ") try/tries! \n"))
         elif difficulty == "Hard" or difficulty == "hard":
                 count += 1
                 if count == 3:
@@ -40,10 +46,10 @@ while again != "no" or again != "No" :
                     break
                 if guessed_number < hidden:
                     print("The number we are looking for is bigger than yours")
-                    guessed_number = int(input("Try again \n"))
+                    guessed_number = int(input("Try again, you still have ("+ str(3 - count) + ") try/tries! \n"))
                 elif guessed_number > hidden:
                     print("The number we are looking for is smaller than yours")
-                    guessed_number = int(input("Try again \n"))
+                    guessed_number = int(input("Try again, you still have ("+ str(3 - count) + ") try/tries! \n"))
 
     if y == 0:
         print("You Won!")
